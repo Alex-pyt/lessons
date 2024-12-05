56d90e61fe0956c90b97154344a7daf6096a5e11
Alex-pyt <cr3gto@gmail.com>
2024-12-05 03:34:49 +0300

    Delete variable.py

--- variable.py
+++
@@ -1,6 +0,0 @@
-tasks = 'number of completed tasks 12,'
-spent = 'number of hours spent 1.5,'
-name = "Training course 'Python',"
-time_for_one_task = (print(1.5 / 12))
-time = 'average time for one task 0.125.'
-print(name, tasks, spent, time)
