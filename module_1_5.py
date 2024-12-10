cf7501badd4341b5027641d25a50efc8cb6035bb
Alex-pyt <cr3gto@gmail.com>
2024-12-11 01:07:37 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 20, 26, False, 'New Year'
+print(immutable_var)
+#immutable_var[1] = 27
+#print(immutable_var]
+explanation = "'Tuple' object does not support item assignment."
+print(explanation)
+mutable_list = [20, 26, False, 'New Year']
+print(mutable_list)
+mutable_list[1] = 25
+mutable_list[-2] = True
+print(mutable_list)
